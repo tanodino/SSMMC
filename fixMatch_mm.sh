5d779889d3a7bfc4421c3707d3f150579ec5d9c0
Dino Ienco <tanodino@gmail.com>
2026-07-17 17:59:53 +0200

    fixMatch_mm.sh

---
+++ fixMatch_mm.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#SBATCH --job-name=pretrain_ours
+#SBATCH --output=pretrain_ours_%j.out
+#SBATCH --error=pretrain_ours_%j.err
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=20
+#SBATCH --hint=nomultithread
+#SBATCH --time=20:00:00
+#SBATCH --qos=qos_gpu-t3
+#SBATCH --constraint=v100-32g
+#SBATCH --account=xfp@v100
+
+module purge
+module load pytorch-gpu/py3/2.3.0
+
+export PYTHONUSERBASE=$WORK/.local
+
+cd $WORK/SSL_ViT/SSL_colearning_S1S2
+
+#../ML-METER_DATA/ SAR MS 9
+#$1: directory of the data
+#$2: First Data Modality
+#$3: Second Data Modality
+#$4: number of patches
+#$5: max number of epochs
+
+#srun python baselines.py $1 $2 $3 $4 $5 $6
+srun python pretrain_MM_smallV4.py $1 $2 $3 $4 $5
