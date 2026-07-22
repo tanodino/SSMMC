60849d9b68953a92576a94bba68b329fa09d4d24
tanodino <dino.ienco@irstea.fr>
2026-07-22 11:56:15 +0000

    proto ssl main

--- proto_ssl_main.py
+++ proto_ssl_main.py
@@ -519,7 +519,7 @@ if __name__ == "__main__":
                     loss_me = mean_entropy_max_loss(probs_strong)
 
                     #loss = loss_sup + LAMBDA_U * (loss_consistency + LAMBDA_ME * loss_me)
-                    loss = loss_sup + LAMBDA_U * loss_consistency + LAMBDA_ME * loss_me
+                    loss = loss_sup + LAMBDA_U * loss_consistency #+ LAMBDA_ME * loss_me
 
                 scaler.scale(loss).backward()
                 scaler.step(optimizer)
