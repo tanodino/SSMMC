aebc7c0e5496dcdd9bea98947cc6bc4ec6fc1adb
tanodino <dino.ienco@irstea.fr>
2026-07-31 12:39:01 +0000

    pretrain abla

---
+++ ours_pretrain_abla_h100.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#SBATCH --job-name=pretrain_ours
+#SBATCH --output=pretrain_ours_%j.out
+#SBATCH --error=pretrain_ours_%j.err
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=20
+#SBATCH --hint=nomultithread
+#SBATCH --time=20:00:00
+#SBATCH --qos=qos_gpu_h100-t3
+#SBATCH --constraint=h100
+#SBATCH --account=xfp@h100
+
+module purge
+module load arch/h100
+module load pytorch-gpu/py3/2.4.0
+
+export PYTHONUSERBASE=$WORK/.local
+
+cd $WORK/SSMMC/SSMMC
+
+#e.g. EUROSAT SAR MS
+#$1: directory of the data
+#$2: First Data Modality
+#$3: Second Data Modality
+
+srun python pretrain_abla.py $1 $2 $3
