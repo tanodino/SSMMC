f14ac0c8b43611cb1781eefc442118a0297251d3
tanodino <dino.ienco@irstea.fr>
2026-08-14 09:05:19 +0000

    ours per Run v5 all layers

--- ours_perRun_v5_allLayers.sh
+++ ours_perRun_v5_allLayers.sh
@@ -5,16 +5,15 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --gres=gpu:1
-#SBATCH --cpus-per-task=24
+#SBATCH --cpus-per-task=20
 #SBATCH --hint=nomultithread
 #SBATCH --time=20:00:00
-#SBATCH --qos=qos_gpu_h100-t3
-#SBATCH --constraint=h100
-#SBATCH --account=xfp@h100
+#SBATCH --qos=qos_gpu-t3
+#SBATCH --constraint=v100-32g
+#SBATCH --account=xfp@v100
 
 module purge
-module load arch/h100
-module load pytorch-gpu/py3/2.4.0
+module load pytorch-gpu/py3/2.3.0
 
 export PYTHONUSERBASE=$WORK/.local
 
