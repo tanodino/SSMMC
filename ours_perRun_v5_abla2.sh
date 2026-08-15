09f329bda9d20e2f40070cbfd54ddbc000ee066f
tanodino <dino.ienco@irstea.fr>
2026-08-15 18:57:57 +0000

    abla sha

---
+++ ours_perRun_v5_abla2.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#SBATCH --job-name=ours
+#SBATCH --output=ours_V5_long_%j.out
+#SBATCH --error=ours_V5_long_%j.err
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=20
+#SBATCH --hint=nomultithread
+#SBATCH --time=20:00:00
+#SBATCH --qos=qos_gpu-t3
+#SBATCH --constraint=v100-32g
+#SBATCH --account=xfp@v100
+
+module purge
+module load pytorch-gpu/py3/2.3.0
+
+export PYTHONUSERBASE=$WORK/.local
+
+cd $WORK/SSMMC/SSMMC
+
+# $1 - Dataset
+# $2 - First Modality
+# $3 - Second Modality
+# $4 - per-class labels
+# $5 - runID
+
+# ABLATION WITHOUT MULTI-LAYER AGGREGATION/FUSION
+
+srun python ssl_pretrained_classif_v5_abla2.py $1 $2 $3 $4 $5 $1/PRETRAIN/checkpoint_latest.pth
